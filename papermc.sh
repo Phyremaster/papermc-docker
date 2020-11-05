18713971b246a19ea4800cb8e8ed87c4b987350a
conrad <52695768+Cookie04DE@users.noreply.github.com>
2020-11-05 20:53:47 +0100

    added check for outdated version when using latest to prevent unnecessary downloads

--- papermc.sh
+++ papermc.sh
@@ -6,16 +6,25 @@ cd papermc
 
 JAR_NAME=papermc-${MC_VERSION}-${PAPER_BUILD}
 
-# Preform initial setup
-if [ ! -e ${JAR_NAME}.jar ] || [ ${PAPER_BUILD} = latest ]
+# Perform initial setup
+outdated=false
+urlPrefix=https://papermc.io/api/v1/paper/${MC_VERSION}
+if [ ${PAPER_BUILD} = latest ]
   then
-    wget https://papermc.io/api/v1/paper/${MC_VERSION}/${PAPER_BUILD}/download -O ${JAR_NAME}.jar
+    outdated= [ "$(wget -q -O- ${urlPrefix}/latest | diff -s latest  -)" != "Files latest and - are identical" ]
+fi
+
+if [ ! -e ${JAR_NAME}.jar ] || ${outdated}
+  then
+    wget ${urlPrefix}/${PAPER_BUILD}/download -O ${JAR_NAME}.jar
     if [ ! -e eula.txt ]
     then
       java -jar ${JAR_NAME}.jar
       sed -i 's/false/true/g' eula.txt
     fi
 fi
+
+wget ${urlPrefix}/latest -O latest
 
 # Start server
 java -server -Xms${MC_RAM} -Xmx${MC_RAM} ${JAVA_OPTS} -jar ${JAR_NAME}.jar nogui
